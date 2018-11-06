40250c145b79dcd878dd97a62a1d6708a38dd3e9
Akhara <43671530+Student2003@users.noreply.github.com>
2018-11-06 10:26:06 +0100

    Add files via upload

---
+++ controller.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: Akhara
+ * Date: 06/11/2018
+ * Time: 08:54
+ */
+
+function suiteDescription() {
+    require "views/description.php";
+}
+function pagePresentation() {
+    require "views/presentation.php";
+}
+
+?>
