060c67fd90eeb87ff47ce7e6bff1d37c16faed89
Gaellerjt <44191104+Gaellerjt@users.noreply.github.com>
2018-11-06 10:28:46 +0100

    Add files via upload

---
+++ connexion.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8"> <!-- UTF-8 permet d'obtenir tous les caractères chinois, arabdes, accents... -->
+		<link rel="stylesheet"  href="connexion.css"/>
+		<title> DomoLink </title>
+</head>
+<body>
+
+	<header>
+		<p> 
+			<img src="logoapp.png" alt="DomoLink" /></br> 
+			<em> DomoLink </em> : le top de la maison connectée ! 
+		</p>
+		
+	</header>
+
+	<nav>
+
+		<section class = "Section1" type ="float:right"> 
+			</br>
+			</br>
+			</br>
+			</br>
+			</br>
+			</br>
+
+			<h2> Connexion </h2>
+			<!--</br></br></br></br>-->
+			<h3> E-mail : </h3>
+				<input type="text" name="e-mail" size="40" required id="uname" style=" height : 30px;" >
+
+			<h3> Mot de passe </h3>
+				<input type="password" name="mot de passe" size="40" id="uname" required style=" height : 30px;"> </br></br>
+				<button style=" size : 40px;height : 40px;" href ="Gérer son compte.html"> Connexion </button> </br> 
+
+			<p> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;  &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;  &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;  &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; <a class = "oublié" href ="Oublié.html"> Mot de passe oublié </a> </p>
+			</br>
+			</br>
+			</br>
+			</br>
+			</br>
+			</br>
+			</br>
+			</br>
+			</br>
+			</br>
+			</br>
+			</br>
+			
+			
+		</section> 
+
+		<section class = "Section2" type ="float:left"> 
+
+			<h2> S'inscrire </h2>
+			<h3> Prénom : </h3>
+				<input type="text" name="prénom" size="40" id="uname" required style=" height : 30px;" >
+			 		</br></br>
+
+			 <h3> Nom : </h3>
+			 	<input type="text" name="nom" size="40" id="uname" required style=" height : 30px;" >
+
+			 <h3> E-mail : </h3>
+			 	<input type="text" name="e-mail" size="40" id="uname" required style=" height : 30px;" >
+			 		</br></br>
+
+			 <h3> Mot de passe : </h3>
+			 	<input type="password" name="mdp" size="40" id="uname" required style=" height : 30px;" >
+			 		</br></br>
+
+			 <h3> Confirmation mot de passe : </h3>
+			 	<input type="password" name="confirmation" size="40" id="uname" required style=" height : 30px;" >&nbsp; &nbsp; &nbsp; &nbsp; </br></br>
+
+			 	
+			 <button style="size : 40px; height: 40px;">Confirmer </button> 
+			 </br>
+			 </br>
+
+		</section> 
+	
+	</nav>
+
+	<footer>
+
+		<p> Nous contacter :              
+			</br>
+		<a href = " mailto:gaellerojat@hotmail.fr">  Envoyer un email </a> </p> 
+	</footer>
+
+
+
+</body>
+</html>
