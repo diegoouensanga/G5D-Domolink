b0f321b6c37dbd39fb5ded60b8d62eea96560bb6
Votre nom ou pseudoPierre <driver.80.18@gmail.com>
2018-11-19 13:38:28 +0100

    "ajout notifications.php"

---
+++ notifications.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Notre première instruction : echo</title>
+        <meta charset="utf-8" />
+    </head>
+
+
+    <?php include("header.php"); ?>
+
+</html>
