4ec43bb3321e2a95863ad01761b0cf7f2cbcc339
Votre nom ou pseudoPierre <driver.80.18@gmail.com>
2018-11-19 14:03:07 +0100

    modification notifications.php

--- notifications.php
+++ notifications.php
@@ -1,11 +1,36 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Notre première instruction : echo</title>
+        <title>Notifications</title>
         <meta charset="utf-8" />
     </head>
 
 
     <?php include("header.php"); ?>
 
+    <body>
+    	
+    	<section>
+    		<h1>Notifications</h1>
+
+    	</section>
+
+
+
+
+
+
+
+    </body>
+
 </html>
+
+<style>
+section{
+	background-color: red;
+	margin-left: 900px;
+}	
+
+
+
+</style>
