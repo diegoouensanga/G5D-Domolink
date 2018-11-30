bfc1fbe0e2a78abbe68f8be686a0b39090d3c711
pierre8018 <driver.80.18@gmail.com>
2018-11-30 10:44:55 +0100

    modif notification

--- notifications.php
+++ notifications.php
@@ -12,6 +12,18 @@
     	
     	<section>
     		<h1>Notifications</h1>
+    		<table>
+    			<thead>
+    				<tr>
+	    				<th>Nom</th>
+	    				<th>Objet</th>
+	    				<th>Date</th>
+    				</tr>
+    			</thead>
+    			<tbody>
+    				
+    			</tbody>
+    		</table>
 
     	</section>
 
@@ -27,10 +39,17 @@
 
 <style>
 section{
-	background-color: red;
+	border: 1px solid black;
 	margin-left: 900px;
+	overflow: auto;
 }	
 
+h1{
+	text-align: center;
+}
 
+table{
+	text-align: justify;
+}
 
 </style>
