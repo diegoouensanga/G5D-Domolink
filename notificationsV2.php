4310a35f27e92e8da155caaf4def109b3ff38cce
pierre8018 <driver.80.18@gmail.com>
2018-12-06 17:08:02 +0100

    Update notificationsV2.php

--- notificationsV2.php
+++ notificationsV2.php
@@ -28,11 +28,16 @@
         	<?php
         while ($donnees = $reponse->fetch()) {
             ?>
-            <button class="buttonA"><span><?php echo $donnees['Provenance'] . ' / ' . $donnees['Objet']. '  ( ' . $donnees['Date envoi'] . ')<br />
-				'; ?></span></button>
+            <button class="buttonA">
+			<span><?php
+
+            echo $donnees['Provenance'] . ' / ' . $donnees['Objet'] . '  ( ' . $donnees['Date envoi'] . ')<br />
+				';
+            ?></span>
+		</button>
                 
 			
-            <?php 
+            <?php
         }
         $reponse->closeCursor();
 
@@ -47,51 +52,59 @@
 
 <style>
 section {
-	border: 1px solid black;
+	border: 1px solid #D52C42;
 	margin-left: 900px;
 	overflow: auto;
+	border-radius: 10px;
 }
 
 h1 {
+	font-family: "Comfortaa-Bold";
+	color: #D52C42;
 	text-align: center;
 }
 
 .buttonA {
-    background-color: #FB5E5E; 
-    border: none;
-    color: white;
-    padding: 15px 32px;
-    text-align: center;
-    text-decoration: none;
-    display: inline-block;
-    font-size: 16px;
-    margin: 4px 2px;
-    cursor: pointer;
+	font-family: "Comfortaa-Regular";
+	background-color: #D46372;
+	border-radius: 5px;
+	color: white;
+	padding: 15px 32px;
+	text-align: center;
+	text-decoration: none;
+	display: inline-block;
+	font-size: 16px;
+	margin: 4px 2px;
+	cursor: pointer;
+	color: white;
 }
+
+.buttonA:hover {
+	background-color: #D52C42;
+}
+
 .buttonA span {
-  cursor: pointer;
-  display: inline-block;
-  position: relative;
-  transition: 0.5s;
+	cursor: pointer;
+	display: inline-block;
+	position: relative;
+	transition: 0.5s;
 }
+
 .buttonA span:after {
-  content: '\00bb';
-  position: absolute;
-  opacity: 0;
-  top: 0;
-  right: -10px;
-  transition: 0.5s;
+	content: '\00bb';
+	position: absolute;
+	opacity: 0;
+	top: 0;
+	right: -10px;
+	transition: 0.5s;
 }
 
 .buttonA:hover span {
-  padding-right: 20px;
+	padding-right: 20px;
 }
 
 .buttonA:hover span:after {
-  opacity: 1;
-  right: 0;
+	opacity: 1;
+	right: 0;
 }
 </style>
-
-
-
