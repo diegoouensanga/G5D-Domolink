3707712dc42d0afa44aa595fa002b2ce3e03b2f5
pierre8018 <driver.80.18@gmail.com>
2018-12-10 19:04:22 +0100

    update notifs

--- notificationsV2.php
+++ notificationsV2.php
@@ -7,8 +7,6 @@
 
 <body>
 
-    <?php include("header.php"); ?>
-    
     
     <?php
     try {
@@ -17,7 +15,7 @@
         die('Erreur : ' . $e->getMessage());
     }
 
-    $reponse = $bdd->query('SELECT * FROM Notifications');
+    $reponse = $bdd->query('SELECT *, TIME_FORMAT (	heure, "%Hh%i") AS heure, DATE_FORMAT (date, "%d/%m/%Y") AS date FROM Notifications ORDER BY id DESC');
 
     ?>
     
@@ -25,7 +23,7 @@
 		<h1>Notifications</h1>
 
         	<?php
-        	if(empty($reponse))
+        	if($reponse->rowCount() == 0)
         	{
         	    echo "<p>Il n'y a pas de nouvelle notification</p>";
         	}
@@ -35,7 +33,7 @@
             	        ?>
                 <button class="buttonA">
     			<span><strong><a href="notifications.php" title="page des notifications"><?php echo $donnees['expediteur']?></strong>
-    			<?php echo $donnees['objet'] . '  (' . $donnees['date'] . ')<br />';
+    			<?php echo $donnees['objet'] . '<br />' . $donnees['heure'] . ' : ' . $donnees['date'] . '<br />';
                 ?></a></span>
         		</button>
                     <?php
@@ -59,6 +57,9 @@ section {
 	margin-left: 900px;
 	overflow: auto;
 	border-radius: 10px;
+	grid-row: 1/4;
+	grid-column: 7/9;
+	margin-right: 3%;
 }
 
 h1 {
@@ -73,6 +74,7 @@ a {
 }
 
 .buttonA {
+    width : 100%;
 	font-family: "Comfortaa-Regular";
 	background-color: #D46372;
 	border-radius: 5px;
@@ -92,6 +94,7 @@ a {
 }
 
 .buttonA span {
+    width : 100%;
 	cursor: pointer;
 	display: inline-block;
 	position: relative;
