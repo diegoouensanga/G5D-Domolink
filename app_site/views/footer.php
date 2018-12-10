349d1b62cf19f029ea035333af8dfe5ea3f554b0
Akhara <43671530+Student2003@users.noreply.github.com>
2018-12-10 14:13:41 +0100

    Finito

--- app_site/views/footer.php
+++ app_site/views/footer.php
@@ -1,39 +1,43 @@
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
 <footer>
     <div class="grille">
         <div class = "location">
-            <h3> Où nous trouver ? </h3>
-            <p>
-                28 rue Notre Dame des Champs </br>
-                75006 Paris
-            </p>
+            <h3> Nous contacter </h3>
+            Adresse : <?php echo $donneesAdmin['adresse']; ?></br>
+            Téléphone : <?php echo $donneesAdmin['telephone']; ?></br>
+            E-Mail: <a style= "color :#0063e3;" href ="mailto:<?php echo $donneesAdmin['mail']; ?>"><?php echo $donneesAdmin['mail']; ?></a></br>
+
         </div>
 
-        <div class ="web">
-            <h3> Suivez-nous sur les réseaux sociaux </h3>
-            <img src="logo%20twitter.png" width="30vw/"> &nbsp;
-            <img src="logo%20fb.png" width="45vw/">&nbsp;
-            <img src="logo%20insta.png" width="30vw/">
+        <div class ="web" style="color:#0063e3;">
+            <h3 style="color :#D52C42;"> Suivez-nous sur les réseaux sociaux </h3>
+            <a href="#" style="color :#0063e3;"><img src="Icons/Twitter.png" width="30vw">Twitter</a><br>
+            <a href="#" style="color :#0063e3;"><img src="Icons/Facebook.png" width="30vw"></a>Facebook<br>
+            <a href="#" style="color :#0063e3;"><img src="Icons/Instagram.png" width="30vw"></a>Instagram
         </div>
 
         <?php if($_SERVER['PHP_SELF'] != "/connexion.php"): ?>
             <div class ="a_propos">
                 <h3>À propos de DomoLink</h3>
-                <p>  Projet développé par DomIsep</br>
-                    À développer... </p>
+                Site de <?php echo $donneesAdmin['nom']; ?></br>
+                Un projet de la société <?php echo $donneesAdmin['societe']; ?>
             </div>
         <?php endif; ?>
     </div>
 </footer>
 
-
 <style>
-    footer
-    {
-    //background-color: #AD031A;
-        height: 20%;
-        width: 100%;
-        margin-top: 50%;
-
+    img{
+        vertical-align: middle;
+    }
+    footer {
+        display: block;
+        max-height : 5%;
+        padding: 4px;
+        margin-top : 5px;
+        color :#D52C42;
+        border-top : 1px solid #D52C42;
     }
     .grille
     {
@@ -41,26 +45,26 @@
         grid-template-columns: repeat(2, 1fr);
         grid-gap: 5px;
         grid-auto-rows: 100%;
-
     }
-
     .location
     {
         grid-column: 1;
         grid-row: 1;
     }
-
     .web
     {
         grid-column: 2;
         grid-row: 1;
-
     }
-
     .a_propos
     {
         grid-column: 3;
         grid-row: 1;
     }
-
+    h3 {
+        font-family: "Comfortaa-Bold";
+        font-weight: bold;
+        margin-top : 0px;
+        margin-bottom : 5px;
+    }
 </style>
