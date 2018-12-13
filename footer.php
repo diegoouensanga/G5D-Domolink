4008fb1dca98419fd3a29fcd74b797009cc236dc
AlexandreMarquis31 <44145726+AlexandreMarquis31@users.noreply.github.com>
2018-12-13 12:02:09 +0100

    Add files via upload

--- footer.php
+++ footer.php
@@ -11,24 +11,52 @@
 
         <div class ="web" style="color:#0063e3;">
             <h3 style="color :#D52C42;"> Suivez-nous sur les réseaux sociaux </h3>
-            <a href="#" style="color :#0063e3;"><img src="ressources/Icons/Twitter.png" width="30vw">Twitter</a><br>
-            <a href="#" style="color :#0063e3;"><img src="ressources/Icons/Facebook.png" width="30vw">Facebook</a><br>
-            <a href="#" style="color :#0063e3;"><img src="ressources/Icons/Instagram.png" width="30vw">Instagram</a>
+            <a href="#" style="color :#0063e3; text-decoration:none;"><img src="ressources/Icons/Twitter.png" width="30vw">Twitter</a><br>
+            <a href="#" style="color :#0063e3; text-decoration:none;"><img src="ressources/Icons/Facebook.png" width="30vw">Facebook</a><br>
+            <a href="#" style="color :#0063e3; text-decoration:none;"><img src="ressources/Icons/Instagram.png" width="30vw">Instagram</a>
         </div>
 
         <?php if($_SERVER['PHP_SELF'] != "/connexion.php"): ?>
             <div class ="a_propos">
                 <h3>À propos de DomoLink</h3>
                 Site de <?php echo $donneesAdmin['nom']; ?></br>
-                    Un projet de la société <?php echo $donneesAdmin['societe']; ?>
+                    Un projet de la société <?php echo $donneesAdmin['societe']; ?><br>
+                <a onclick="displayMentions()" style="color :#D52C42; cursor: pointer;">Conditions Générales d'utilisation</a><br>
+                <a onclick="displayCGU()" style="color :#D52C42; cursor: pointer;">Mentions Légales</a><br>
             </div>
         <?php endif; ?>
     </div>
 </footer>
 
+<script>
+    function displayMentions() {
+        var myWindow = window.open("", "DomoLink", "width=2000,height=1000");
+        myWindow.document.write("<!DOCTYPE html>\n" +
+            "<head>\n" +
+            "  <meta charset=\"UTF-8\">\n" +
+            "  <title>Mentions Légales</title>\n" +
+            "</head>");
+        myWindow.document.write("<?php
+            $cgu = preg_replace("/\r\n|\r|\n/",'<br/>',$donneesAdmin['mentions_legales']);
+            echo $cgu;
+            ?>");
+    }
+    function displayCGU() {
+        var myWindow = window.open("", "DomoLink", "width=2000,height=1000");
+        myWindow.document.write("<!DOCTYPE html>\n" +
+            "<head>\n" +
+            "  <meta charset=\"UTF-8\">\n" +
+            "  <title>Conditions Générales d'utilisation</title>\n" +
+            "</head>");
+        myWindow.document.write("<?php
+            $cgu = preg_replace("/\r\n|\r|\n/",'<br/>',$donneesAdmin['cgu']);
+            echo $cgu;
+            ?>");
+    }
+</script>
 <style>
-    a {
-        text-decoration: none;
+    a{
+        text-decoration: underline;
     }
     img{
         vertical-align: middle;
