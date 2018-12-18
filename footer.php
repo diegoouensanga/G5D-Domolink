698bc83d706dbc3f4809b36d57a37998feff4347
AlexandreMarquis31 <44145726+AlexandreMarquis31@users.noreply.github.com>
2018-12-18 08:47:02 +0100

    Add files via upload

--- footer.php
+++ footer.php
@@ -1,102 +1,28 @@
 </div>
 <footer>
-    <div class="grille">
-        <div class = "location">
-            <h3> Nous contacter </h3>
-                Adresse : <?php echo $donneesAdmin['adresse']; ?></br>
-                Téléphone : <?php echo $donneesAdmin['telephone']; ?></br>
-                E-Mail: <a style= "color :#0063e3;" href ="mailto:<?php echo $donneesAdmin['mail']; ?>"><?php echo $donneesAdmin['mail']; ?></a></br>
-
-        </div>
-
-        <div class ="web" style="color:#0063e3;">
-            <h3 style="color :#D52C42;"> Suivez-nous sur les réseaux sociaux </h3>
-            <a href="#" style="color :#0063e3; text-decoration:none;"><img src="ressources/Icons/Twitter.png" width="30vw">Twitter</a><br>
-            <a href="#" style="color :#0063e3; text-decoration:none;"><img src="ressources/Icons/Facebook.png" width="30vw">Facebook</a><br>
-            <a href="#" style="color :#0063e3; text-decoration:none;"><img src="ressources/Icons/Instagram.png" width="30vw">Instagram</a>
-        </div>
-
-        <?php if($_SERVER['PHP_SELF'] != "/connexion.php"): ?>
-            <div class ="a_propos">
-                <h3>À propos de DomoLink</h3>
-                Site de <?php echo $donneesAdmin['nom']; ?></br>
-                    Un projet de la société <?php echo $donneesAdmin['societe']; ?><br>
-                <a onclick="displayMentions()" style="color :#D52C42; cursor: pointer;">Conditions Générales d'utilisation</a><br>
-                <a onclick="displayCGU()" style="color :#D52C42; cursor: pointer;">Mentions Légales</a><br>
-            </div>
-        <?php endif; ?>
+    <div class="footer_location">
+        <h3> Nous contacter </h3>
+        Adresse : <?php echo $donneesAdmin['adresse']; ?><br>
+        Téléphone : <?php echo $donneesAdmin['telephone']; ?><br>
+        E-Mail: <a style="color :#0063e3;"
+                   href="mailto:<?php echo $donneesAdmin['mail']; ?>"><?php echo $donneesAdmin['mail']; ?></a><br>
+    </div>
+    <div class="footer_social">
+        <h3> Suivez-nous sur les réseaux sociaux </h3>
+        <a href=<?php echo $donneesAdmin['twitter']; ?>><img src="ressources/Icons/Twitter.png" width="30vw"
+                                                             alt="Twitter Icon">Twitter</a><br>
+        <a href=<?php echo $donneesAdmin['facebook']; ?>><img src="ressources/Icons/Facebook.png" width="30vw"
+                                                              alt="Facebook Icon">Facebook</a><br>
+        <a href=<?php echo $donneesAdmin['instagram']; ?>><img src="ressources/Icons/Instagram.png" width="30vw"
+                                                               alt="Instagram Icon">Instagram</a>
+    </div>
+    <div class="footer_description">
+        <h3>À propos de DomoLink</h3>
+        Site de <?php echo $donneesAdmin['nom']; ?><br>
+        Un projet de la société <?php echo $donneesAdmin['societe']; ?><br>
+        <a onclick="display('mentions_legales')">Conditions Générales d'utilisation</a><br>
+        <a onclick="display('cgu')">Mentions Légales</a><br>
     </div>
 </footer>
 
-<script>
-    function displayMentions() {
-        var myWindow = window.open("", "DomoLink", "width=2000,height=1000");
-        myWindow.document.write("<!DOCTYPE html>\n" +
-            "<head>\n" +
-            "  <meta charset=\"UTF-8\">\n" +
-            "  <title>Mentions Légales</title>\n" +
-            "</head>");
-        myWindow.document.write("<?php
-            $cgu = preg_replace("/\r\n|\r|\n/",'<br/>',$donneesAdmin['mentions_legales']);
-            echo $cgu;
-            ?>");
-    }
-    function displayCGU() {
-        var myWindow = window.open("", "DomoLink", "width=2000,height=1000");
-        myWindow.document.write("<!DOCTYPE html>\n" +
-            "<head>\n" +
-            "  <meta charset=\"UTF-8\">\n" +
-            "  <title>Conditions Générales d'utilisation</title>\n" +
-            "</head>");
-        myWindow.document.write("<?php
-            $cgu = preg_replace("/\r\n|\r|\n/",'<br/>',$donneesAdmin['cgu']);
-            echo $cgu;
-            ?>");
-    }
-</script>
-<style>
-    a{
-        text-decoration: underline;
-    }
-    img{
-        vertical-align: middle;
-    }
-    footer {
-        border-top : 1px solid #D52C42;
-        color :#D52C42;
-        width: calc(100% - 8px);
-        position: absolute;
-        left: 0;
-        padding: 4px;
-        font-size : 1vw;
-    }
-    .grille
-    {
-        display: grid;
-        grid-template-columns: repeat(2, 1fr);
-        grid-gap: 5px;
-        grid-auto-rows: 100%;
-    }
-    .location
-    {
-        grid-column: 1;
-        grid-row: 1;
-    }
-    .web
-    {
-        grid-column: 2;
-        grid-row: 1;
-    }
-    .a_propos
-    {
-        grid-column: 3;
-        grid-row: 1;
-    }
-    h3 {
-        font-family: "Comfortaa-Bold";
-        font-weight: bold;
-        margin-top : 0px;
-        margin-bottom : 5px;
-        font-size : 2vw;
-    }
-</style>
+<script src="javascript/infoformationView.js"></script>
--- header.php
+++ header.php
@@ -1,112 +1,52 @@
-<!DOCTYPE html>
-<head>
-  <meta charset="UTF-8">
-  <link rel="shortcut icon" href="ressources/favicon.png"/>
-  <link rel="stylesheet" href="cssGeneral.css">
-  <meta name="description" content="Le top de la maison Connectée !">
-  <title>DomoLink</title>
-</head>
 <?php
-  require_once("fonctions.php");
-  session_start();
-  if(empty($_SESSION['id']) && !($_SERVER['REQUEST_URI']== "/connexion.php")){
+require_once("fonctions.php");
+session_start();
+if (empty($_SESSION['id']) && !($_SERVER['REQUEST_URI'] == "/connexion.php")) {
     header("Location:/connexion.php");
-  }
-  else {
-    $req2 = Database::execute('SELECT nom,societe,telephone,slogan,adresse,mail,cgu,mentions_legales FROM administration',null);
+} else {
+    $req2 = Database::execute('SELECT nom,societe,telephone,slogan,adresse,mail,facebook,twitter,instagram FROM administration', null);
     $donneesAdmin = $req2->fetch();
 }
 ?>
 
 <body>
-<div class="contenu">
 <header>
-  <div class="topIcon">
-    <a href="gererSonDomicile.php?piece=VueGenerale" style= "text-decoration: none;">
-      <img draggable="false" src="ressources/Logo.png" alt="DomoLink" width=200vw/>
-    </a>
-    <div class="slogan"></br><em class="slogan compagnie"><?php echo $donneesAdmin['nom']; ?></em> : <?php echo $donneesAdmin['slogan']; ?></div>
-  </div>
-  <?php if($_SERVER['PHP_SELF'] != "/connexion.php"): ?>
-  <div class="topMenu">
-    <?php if($_SESSION['type'] == 0): ?>
-    <div class="menuItem">
+    <div class="topIcon">
+        <a href="dashBoard.php?piece=VueGenerale" style="text-decoration: none;">
+            <img draggable="false" src="ressources/Logo.png" alt="DomoLink" width=200vw/>
+        </a>
+        <div class="slogan"><br><em class="slogan compagnie"><?php echo $donneesAdmin['nom']; ?></em>
+            : <?php echo $donneesAdmin['slogan']; ?></div>
     </div>
+    <?php if ($_SERVER['PHP_SELF'] != "/connexion.php"): ?>
+        <div class="topMenu">
+            <?php if ($_SESSION['type'] == 0): ?>
+                <div class="menuItem">
+                </div>
+            <?php endif; ?>
+            <div class="menuItem">
+                <a href="dashBoard.php?piece=VueGenerale"><img draggable="false" alt="Domicile Icône"
+                                                               src="ressources/accueil.png" width=60%/></a>
+                <a href="dashBoard.php?piece=VueGenerale" class="caption">Mon Domicile</a>
+            </div>
+            <?php if ($_SESSION['type'] != 0): ?>
+                <div class="menuItem">
+                    <a href="administration.php"><img draggable="false" src="ressources/administration.png"
+                                                      alt="Administration Icône"
+                                                      width=60%/></a>
+                    <a href="administration.php" class="caption">Gérer</a>
+                </div>
+            <?php endif; ?>
+            <div class="menuItem">
+                <a href="compte.php?action=infos"><img draggable="false" src="ressources/compte.png"
+                                                       alt="Icône Compte" width=60%/></a>
+                <a href="compte.php?action=infos" class="caption">Compte</a>
+            </div>
+            <div class="menuItem">
+                <a href=""><img draggable="false" src="ressources/aide.png" alt="DomoLink" width=60%/></a>
+                <a href="" class="caption">Aide</a>
+            </div>
+        </div>
     <?php endif; ?>
-    <div class="menuItem">
-      <a href="gererSonDomicile.php?piece=VueGenerale"><img draggable="false" href="" src="ressources/accueil.png" width = 60%/></a>
-      <a href="gererSonDomicile.php?piece=VueGenerale" class="caption" >Mon Domicile</a>
-    </div>
-    <?php if($_SESSION['type'] != 0): ?>
-    <div class="menuItem">
-      <a href="administration.php"><img  draggable="false" src="ressources/administration.png" width =60%/></a>
-      <a href="administration.php" class="caption" >Gérer</a>
-    </div>
-    <?php endif; ?>
-    <div class="menuItem">
-      <a href="compte.php?action=infos"><img href="default.asp" draggable="false" href="" src="ressources/compte.png" width =60%/></a>
-      <a href="compte.php?action=infos" class="caption" >Compte</a>
-    </div>
-    <div class="menuItem">
-      <a href="index.php"><img draggable="false" src="ressources/aide.png" alt="DomoLink" width =60%/></a>
-      <a href="index.php"  class="caption">Aide</a>
-    </div>
-  </div>
-  <?php endif; ?>
 </header>
-<style>
-    html {
-        height: 100%;
-    }
-    body {
-        min-height: 100%;
-    }
-    .contenu {
-        min-height:calc(100vh - 240px);
-        height:100%;
-        padding-bottom: 120px;
-    }
-header
-{
-  display : flex;
-  margin-bottom:20px;
-    padding: 4px;
-}
-.topIcon {
-  display: inline;
-  float: left;
-}
-.compagnie{
-  color : #EF0A0A;
-}
-.slogan{
-  font-family: "Comfortaa-Regular";
-  width : 100%;
-  font-size: 9px;
-  white-space: nowrap;
-  overflow: hidden;
-}
-.topMenu {
-  display:flex;
-  align-items:center;
-  margin-left:auto;
-  width : 360px;
-  min-width : 120px;
-}
-.menuItem {
-  vertical-align: middle;
-  width : 25%;
-  display : table-cell;
-  text-align:center;
-  transform: translateY(5%);
-}
-.caption {
-  font-family: "Comfortaa-Regular";
-  display : block;
-  font-weight:bold;
-  color:#EF0A0A;
-  font-size: 12px;
-  cursor: pointer;
-  text-decoration: none;
-}
-</style>
+<div class="contenu">
