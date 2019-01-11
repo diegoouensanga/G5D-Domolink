ee90270c0bf421877ddb862d8beadf7af8f94714
AlexandreMarquis31 <44145726+AlexandreMarquis31@users.noreply.github.com>
2019-01-11 16:20:58 +0100

    Add files via upload

--- connexion.php
+++ connexion.php
@@ -1,8 +1,9 @@
 <!DOCTYPE html >
 <head>
     <meta charset="utf-8"> <!-- UTF-8 permet d'obtenir tous les caractères chinois, arabes, accents... -->
+    <meta http-equiv="Content-Security-Policy" content="default-src 'self'">
     <link rel="stylesheet" href="css/cssGeneral.css"/>
-    <link rel="stylesheet" href="connexion.css"/>
+    <link rel="stylesheet" href="css/connexion.css"/>
     <link rel="stylesheet" href="css/header.css"/>
     <link rel="stylesheet" href="css/footer.css"/>
     <title> DomoLink </title>
@@ -19,7 +20,7 @@ if (isset($_POST['connexion'])) {
         $_SESSION['type'] = $donnee['type'];
         header('Location:/dashBoard.php');
     } else {
-        $erreur= 'Identifiant / Mot de passe incorrect !';
+        $erreur = 'Identifiant / Mot de passe incorrect !';
     }
 }
 if ($_POST['inscription']) {
@@ -28,20 +29,18 @@ if ($_POST['inscription']) {
         $existeM = $testmail->fetch();
         $testcMAC = Database::execute('SELECT id,type FROM Utilisateurs WHERE cMAC = :cMAC', array('cMAC' => $_POST['cMAC']));
         $existeMAC = $testcMAC->fetch();
-        if ($existeM){
-            $alreadyMail=  'Vous avez déjà un compte';
-        }
-        else if ($existeMAC){
-            $alreadycMAC= 'Vous avez déjà un compte';
-        }
-        else if ($_POST['mdp'] !== $_POST['confirmation']){
-            $wrongmdp= 'les mots de passe ne correspondent pas, veuillez rééssayer';
-        } else if (strlen($_POST['mdp'])<7){
-            $noSize='Mot de passe trop court';
+        if ($existeM) {
+            $alreadyMail = 'Vous avez déjà un compte';
+        } else if ($existeMAC) {
+            $alreadycMAC = 'Vous avez déjà un compte';
+        } else if ($_POST['mdp'] !== $_POST['confirmation']) {
+            $wrongmdp = 'les mots de passe ne correspondent pas, veuillez rééssayer';
+        } else if (strlen($_POST['mdp']) < 7) {
+            $noSize = 'Mot de passe trop court';
         } else if (!preg_match('#^(?=.*[a-z])(?!=.*[A-Z])(?=.*[0-9])(?=.*\W).{6,}$#', $_POST['mdp'])) {
-            $noConform= 'Mot de passe non conforme';
-        }else if (filter_var($_POST['mail'], FILTER_VALIDATE_EMAIL) === false) {
-            $noMail= 'adresse email non valide';
+            $noConform = 'Mot de passe non conforme';
+        } else if (filter_var($_POST['mail'], FILTER_VALIDATE_EMAIL) === false) {
+            $noMail = 'adresse email non valide';
         } else {
             $req = Database::execute('INSERT INTO Utilisateurs(mdp,mail,cMAC) VALUES(:mdp,:mail,:cMAC)', array('mail' => $_POST['mail'], 'cMAC' => $_POST['cMAC'], 'mdp' => hash('sha256', $_POST['mdp'])));
             $req2 = Database::execute('SELECT LAST_INSERT_ID() AS last_id FROM Utilisateurs');
@@ -62,22 +61,21 @@ if ($_POST['inscription']) {
         <div class="sousection1">
             <form method="post">
                 <h2> Connexion </h2>
-
-                <div class = "erreur">
-                    <?php if($erreur) {
+                <div>
+                    <?php if (isset($erreur)) {
                         echo $erreur;
-                    }?>
+                    } ?>
                 </div>
                 <!--</br></br></br></br>-->
                 <h3> Adresse e-mail : </h3>
-                <input type="text" name="mail" size="40" required id="mailcon" style=" height : 30px;">
+                <input type="text" name="mail" size="40" required id="mailcon">
 
                 <h3> Mot de passe </h3>
-                <input type="password" name="mdp" size="40" id="mdpcon" required style=" height : 30px;"> <br><br>
-                <input name="connexion" type="submit" id="connexion" style=" size : 40px;height : 40px;"/>  <br> <br>
+                <input type="password" name="mdp" size="40" id="mdpcon" required > <br><br>
+                <input class="button conButton" name="connexion" type="submit" id="connexion" /> <br> <br>
             </form>
 
-            <a class="oublié" href="oublie.php"> Mot de passe oublié </a>
+            <a class="oubli" href="oublie.php"> Mot de passe oublié </a>
 
         </div>
 
@@ -87,22 +85,24 @@ if ($_POST['inscription']) {
         <form method="post">
             <h2> S'inscrire </h2>
 
-            <div class = "dejaCompte">
-                <?php if($alreadyMail||$alreadycMAC){
-                    echo $alreadycMAC;
+            <div>
+                <?php if (isset($alreadyMail)){
                     echo $alreadyMail;
+                }
+                if (isset($alreadycMAC)) {
+                    echo $alreadycMAC;
                 }
                 ?>
             </div>
 
             <h3> Numéro cMAC : </h3>
-            <input type="text" name="cMAC" size="40" id="cMAC" required style=" height : 5%;">
+            <input type="text" name="cMAC" size="40" id="cMAC" required >
             <br>
 
             <h3> E-mail : </h3>
-            <input type="text" name="mail" size="40" id="mail" required style=" height : 5%;"><br>
-            <div class = "noMail">
-                <?php if ($noMail){
+            <input type="text" name="mail" size="40" id="mail" required ><br>
+            <div class="errorMsg">
+                <?php if (isset($noMail)) {
                     echo $noMail;
                 }
                 ?>
@@ -110,16 +110,13 @@ if ($_POST['inscription']) {
             <br>
 
             <h3> Mot de passe : </h3>
-            <input type="password" name="mdp" size="40" id="mdp" required style=" height : 5%;"><br>
-            <div class = "tropCourt">
-                <?php if ($noSize){
+            <input type="password" name="mdp" size="40" id="mdp" required ><br>
+            <div class="errorMsg">
+                <?php if (isset($noSize)) {
                     echo $noSize;
                 }
-                ?>
-            </div>
-            <div class = "noConform">
-                <?php if($noConform){
-                    echo $noConform;
+                if (isset($noConform)) {
+                echo $noConform;
                 }
                 ?>
             </div>
@@ -128,31 +125,25 @@ if ($_POST['inscription']) {
 
             <h3> Confirmation mot de passe : </h3>
             <input type="password" name="confirmation" size="40" id="confirmation" required
-                   style=" height : 5%;"> <br>
-            <div class = "wrongMdp">
-                <?php if ($wrongmdp){
+                   > <br>
+            <div >
+                <?php if (isset($wrongmdp)) {
                     echo $wrongmdp;
                 }
                 ?>
-            </div><br>
+            </div>
+            <br>
 
             <input type="checkbox" id="cgu" unchecked name='cgu' required="required">
-            <label for="CGU" class="Cgu"> <a class="cgu" onclick="display('cgu')"> J'accepte les conditions
+            <label for="CGU" class="Cgu"> <a class="cgu" id="cgu"> J'accepte les conditions
                     d'utilisations </a>
             </label> <br><br>
-            <input type = "hidden" name = "inscription" value = "hock">
-            <input name="inscription" id="inscription" type="submit" style="size : 40px; height: 40px;"/>  </br>
+            <input type="hidden" name="inscription" value="hock">
+            <input class="button conButton" name="inscription" id="inscription" type="submit" />  <br>
         </form>
 
     </div>
 
 </div>
-
-<?php
-include 'footer.php';
-?>
-
-
-
-
-
+<script src="javascript/informationView.js"></script>
+<?php include 'footer.php';  ?>
