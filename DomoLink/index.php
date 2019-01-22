4e4e8f9e68fc25cc6aadddd2cf2d6338d4de5918
Alexandre Marquis <alexandre.marquis31@gmail.com>
2019-01-22 12:31:15 +0100

    Reparation

--- DomoLink/index.php
+++
@@ -1,3 +0,0 @@
-<?php
-    header("Location: dashBoard.php?piece=VueGenerale");
-    die();
--- DomoLink/javascript/generalJS.js
+++
@@ -1,4 +0,0 @@
-function checkNum(elmnt) {
-    elmnt.value = elmnt.value.replace(/(?![0-9])./gmi, '');
-}
-
